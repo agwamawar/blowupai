6753c293771afbaa9f4a729dad766758a5e39b04
agwamawar <viktorawar@gmail.com>
2025-07-17 14:00:59 +0100

    Update ExploreOtherCreators.tsx

--- src/components/ExploreOtherCreators.tsx
+++ src/components/ExploreOtherCreators.tsx
@@ -2,13 +2,13 @@ import React from 'react';
 
 const ExploreOtherCreators: React.FC = () => {
   const videoUrls = [
-    'https://www.blowupai.com/%40daveramsey%20STILL%20Hates%20Debt.mp4',
-    'https://www.blowupai.com/A_thing_happened_last_night_Welcome_to_my_sis_%40jackiehillperry_to.mp4',
-    'https://www.blowupai.com/Attachment_Style_Studios_Present_Anxious_and_Avoidant_Wedding_%40.mp4',
-    'https://www.blowupai.com/I%E2%80%99m_taking_small_steps_on_this_journey_because_any_progress%2C_no.mp4',
-    'https://www.blowupai.com/What_an_insane_saga_For_those_of_you_who_weren%E2%80%99t_refreshing_X_all.mp4',
-    'https://www.blowupai.com/a8573291ac375a56863a0dbe2f12d387.mp4',
-    'https://www.blowupai.com/videoplayback%20(4).mp4',
+    'https://videos.blowupai.com/%40daveramsey%20STILL%20Hates%20Debt.mp4',
+    'https://videos.blowupai.com/A_thing_happened_last_night_Welcome_to_my_sis_%40jackiehillperry_to.mp4',
+    'https://videos.blowupai.com/Attachment_Style_Studios_Present_Anxious_and_Avoidant_Wedding_%40.mp4',
+    'https://videos.blowupai.com/I%E2%80%99m_taking_small_steps_on_this_journey_because_any_progress%2C_no.mp4',
+    'https://videos.blowupai.com/What_an_insane_saga_For_those_of_you_who_weren%E2%80%99t_refreshing_X_all.mp4',
+    'https://videos.blowupai.com/a8573291ac375a56863a0dbe2f12d387.mp4',
+    'https://videos.blowupai.com/videoplayback%20(4).mp4',
   ];
 
   return (
